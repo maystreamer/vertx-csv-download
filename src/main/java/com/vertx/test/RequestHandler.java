91462333e7abb0ad16171c14a7e48cd1a025c51e
saubkukreti@gmail.com <saubkukreti@gmail.com>
2018-05-15 17:22:43 +0530

    updated for the query params

--- src/main/java/com/vertx/test/RequestHandler.java
+++ src/main/java/com/vertx/test/RequestHandler.java
@@ -12,46 +12,45 @@ import io.vertx.core.streams.ReadStream;
 import io.vertx.ext.web.RoutingContext;
 
 public class RequestHandler implements Handler<RoutingContext> {
-	private Vertx vertx;
+    private Vertx vertx;
 
-	public RequestHandler(Vertx vertx) {
-		this.vertx = vertx;
-	}
+    public RequestHandler(Vertx vertx) {
+        this.vertx = vertx;
+    }
 
-	@Override
-	public void handle(RoutingContext event) {
-		DeliveryOptions options = new DeliveryOptions();
-		HttpServerResponse response = event.response();
-		response.putHeader(HttpHeaders.CONTENT_TYPE, "application/csv")
-				.putHeader("Content-Disposition", "attachment; filename=user_details.csv")
-				.putHeader(HttpHeaders.TRANSFER_ENCODING, "chunked").setChunked(true);
+    @Override
+    public void handle(RoutingContext event) {
+        DeliveryOptions options = new DeliveryOptions();
+        HttpServerResponse response = event.response();
+        response.putHeader(HttpHeaders.CONTENT_TYPE, "application/csv")
+                .putHeader("Content-Disposition", "attachment; filename=user_details.csv")
+                .putHeader(HttpHeaders.TRANSFER_ENCODING, "chunked").setChunked(true);
 
-		// this.vertx.eventBus().consumer("4f127476-41c8-4d55-97fa-6e0c98f27e74", result
-		// -> {
-		// JsonArray array = (JsonArray) result.body();
-		// response.write(toString(array), "UTF-8");
-		// });
+        // this.vertx.eventBus().consumer("4f127476-41c8-4d55-97fa-6e0c98f27e74", result
+        // -> {
+        // JsonArray array = (JsonArray) result.body();
+        // response.write(toString(array), "UTF-8");
+        // });
 
-		ReadStream<Buffer> consumer = this.vertx.eventBus().<Buffer>consumer("4f127476-41c8-4d55-97fa-6e0c98f27e74")
-				.bodyStream();
-		consumer.handler(result -> {
-			JsonArray array = (JsonArray) result;
-			response.write(toString(array), "UTF-8");
-		});
+        ReadStream<Buffer> consumer = this.vertx.eventBus().<Buffer>consumer("4f127476-41c8-4d55-97fa-6e0c98f27e74")
+                .bodyStream();
+        consumer.handler(result -> {
+            JsonArray array = (JsonArray) result;
+            response.write(toString(array), "UTF-8");
+        });
 
-		Pump pump = Pump.pump(consumer, response);
-		this.vertx.eventBus().send(DatabaseHandler.ADDRESS, "hi, from RequestHandler", options, result -> {
-			if(result.succeeded()){
-				response.end();
-				pump.stop();
-			}
-			// response.close();
-		});
-		pump.start();
-	}
+        Pump pump = Pump.pump(consumer, response);
+        this.vertx.eventBus().send(DatabaseHandler.ADDRESS, "hi, from RequestHandler", options, result -> {
+            if (result.succeeded()) {
+                response.end();
+                pump.stop();
+            }
+        });
+        pump.start();
+    }
 
-	public String toString(final JsonArray data) {
-		return String.join(",", "" + data.getValue(0), "" + data.getValue(1), "" + data.getValue(2),
-				"" + data.getValue(3), "" + data.getValue(4), "" + data.getValue(5), "" + data.getValue(6), "\r\n");
-	}
+    public String toString(final JsonArray data) {
+        return String.join(",", "" + data.getValue(0), "" + data.getValue(1), "" + data.getValue(2),
+                "" + data.getValue(3), "" + data.getValue(4), "" + data.getValue(5), "\r\n");
+    }
 }
